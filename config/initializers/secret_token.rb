1e02d585348d39f9faba44efc3736a7615af5c29
Enrico Bernardi <enrico.bernardi@gmail.com>
2012-02-26 11:46:13 +0100

    ofa

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ofa::Application.config.secret_token = '0e2186c2c7121d690b320dd8c18d1f8ff4694924d37b1dfe53a11a0628cb14e502af3c1d160cd74feb9afdfee77c35e7c8babdc66dd733f42daa2e9313c203d0'
